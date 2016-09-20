7726352479dc587c3af4fae635b865e946358ae1
TAKUMI-ABE <basara.ysb@gmail.com>
2016-09-20 16:32:01 +0900

    make program

--- ga_equation_solver/ga_equation_solver/ga_util.h
+++ ga_equation_solver/ga_equation_solver/ga_util.h
@@ -1,138 +1,332 @@
 #ifndef GA_UTIL_H
 #define GA_UTIL_H
 
-#include<memory>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
 
-/* GAで使われるパラメータをまとめて記述するファイル */
-#define F_X pow((x),2)-9 /* 探索する関数。関数への入力はx, 関数からの出力はy */
-#define G_Y 1/(1+fabs(y))  /* 関数の出力値に対しての適合度。 適合度は 高いほど良いものと扱われる。f(x)=0が最良 */
-#define GRAY 1             /* グレイコードかバイナリコードの指定。グレイコードなら1, バイナリコードなら0 */
+// params of GA
+const double PTYPE_MAX = 5.0;
+const double PTYPE_MIN = -5.0;
+const int GTYPE_LEN = 20;
+const int GTYPE_MAX = 1;
+const int POP = 10;
+const int NUM_OF_GENERATION = 100;
 
-/* GAのパラメータ群 */
-#define MAX (5.12)        /* 扱う実数の最大値 */
-#define MIN (-5.12)       /* 扱う実数の最小値 */
-#define LENGTH (10)        /* 遺伝子のコード長 */
-#define POP 100            /* 個体数 */
-#define CODE_MAX 1         /* 各遺伝子コードの最大値。これが１ならコードは0か1になる。ビットストリングの場合は１で固定 */
-#define GAP 0.9            /* 一回の生殖で子供と入れ替わる割合 */
-#define ELITE_RATE 1.0     /* そのまま残る数のうち、エリートの割合 */
-#define P_MUTATE 0.0      /* 突然変異率。LENGTHの逆数程度がよい */
-#define P_CROSS 1.0        /* 交叉確率 */
-#define GENERATION 10       /* GAを計算する世代数 */
-#define SELECTION_METHOD 2 /* 1はルーレット 2はトーナメント*/
-#define TOURNAMENT_SIZE 5	/* トーナメントサイズ。トーナメントの時だけ意味がある  */
+// params of GA Operators
+const double ELITE_RATE = 0.1;
+const double MUTATE_RATE = 0.02;
 
-/* 出力 */
-#define PRINT_GROUP 1
-#define PRINT_FITNESS 1
+// GA Farget Function
+double fx(double x) {
+	return pow((x), 2) - 9.0;
+}
 
-// Define the type of Genotype of GA
-using gtype_t = int*;
-
-/*
- * GAの個体を表す構造体ga_individualを宣言し,.
- * 個体は連結リストになっている.
- */
-using individual_t = class ga_individual*;
-//using individual_t = std::unique_ptr<struct ga_individual>;
-class ga_individual {
-public:
-	gtype_t gtype; // Geno Type
-	double ptype; // Pheno Type
-	double fitness;
-	individual_t next; // pointer to the next indivisual
-	int rank;  // rank after sorting
-	int parent1; // index of parent 1
-	int parent2; // index of parent 2
-	int cross_point; // crossover point
-};
-
-/* 
- * generation
- */
-using ga_population_t = class ga_population*;
-class ga_population {
-public:
-	individual_t genes;  // lead pointer to the invividual list
-	double * pselect; // array of fitness
-	int mutate_count;  // total number of mutation
-	double max_fitness;
-	double min_fitness;
-	double avg_fitness;
-	int population_size; // number of indivisuals
-	int code_length; // length of gene
-	int code_max; // maximum value of genetic Lucus
-};
-
-/*
-* ptype to gtype
-*/
-void encode_gtype(double value, gtype_t gtype, int code_length, double min, double max)
-{
-	double gap = max - min;
-	double remain_value = value - min; /* 値のうち、遺伝子によって表現されている部分 */
-	double value_of_code;         /* その桁の遺伝子が表現する値 */
-	int position = 1;
-	int pre_code = 0;
-	int i = 0;
-	int tmp;  /* グレイコード変換用,一時保管変数 */
-	while (i < code_length) {
-		value_of_code = gap / pow(2, position);
-		if (remain_value >= value_of_code) {
-			gtype[i] = 1;
-			remain_value -= value_of_code;
-		}
-		else {
-			gtype[i] = 0;
-		}
-		/* グレイコードへの変換
-		* バイナリコードと、元のバイナリを右に１シフトしたもののXORをとる
-		*/
-		if (GRAY == 1) {
-			tmp = gtype[i];
-			gtype[i] = (pre_code) ^ (gtype[i]);
-			pre_code = tmp;
-		}
-		position++;
-		i++;
-	}
-	return;
+// GA Fitness Function
+double gy(double y) {
+	return 1.0 / (1.0 + fabs(y));
 }
 
 /*
-* gtype to ptype
-*/
-double decode_gtype(gtype_t gtype, int code_length, double min, double max)
-{
-	double gap = max - min;
-	double decoded_value = min;
-	int position = 1;
-	int pre_code = 0;                     /* １つ上位の桁のコード(バイナリ),バイナリとグレイの変換に必要 */
+ * Geno Type
+ */
+class Gtype {
+public:
+	Gtype() {};
+
+	void setGtypeRamdom(int gtypeCodeLength, int gtypeCodeMax);
+	int mutateGtype();
+	int getGCodeLength() { return gCodeLength; }
+	int getGCodeMax() { return gCodeMax; }
+
+	Gtype operator =(Gtype& g) {
+		this->gVector = g.gVector;
+		this->gCodeLength = g.getGCodeLength();
+		this->gCodeMax = g.getGCodeMax();
+		return *this;
+	}
+
+	std::vector<int> gVector;
+
+private:
+	int gCodeLength;
+	int gCodeMax;
+};
+
+void Gtype::setGtypeRamdom(int gtypeCodeLength, int gtypeCodeMax){
+	gCodeLength = gtypeCodeLength;
+	gCodeMax = gtypeCodeMax;
+	gVector.resize(gtypeCodeLength);
+
+	for (int i = 0; i < gtypeCodeLength; i++)
+		gVector[i] = rand() % (gtypeCodeMax + 1);
+}
+
+int Gtype::mutateGtype(){
+	int mutateCount = 0;
+	int mutateFlag = 0;
+
+	for (int i = 0; i < gCodeLength; i++) {
+		if (((double)rand() / RAND_MAX) <= MUTATE_RATE) {
+			gVector[i] = (gVector[i] == 1)? 0 : 1;
+			mutateCount++;
+		}
+	}
+
+	return mutateCount;
+}
+
+/*
+ * Pheno Type
+ */
+class Ptype {
+public:
+	Ptype() {};
+
+	void decodeGtype(Gtype& g);
+	void setRange(double min, double max);
+	void setPtype(double ptype) { p = ptype; };
+	double getPtype() { return p; };
+
+private:
+	double p;
+	double pMin;
+	double pMax;
+};
+
+void Ptype::decodeGtype(Gtype& g) {
+	double gap = pMax - pMin;
+	double decoded_value = pMin;
 
 	int i = 0;
-	/* グレイコードの解釈 */
-	/* 変換されたバイナリの１つ上位の桁のコードとの排他的論理和を取る */
-	if (GRAY == 1) {
-		while (i < code_length) {
-			pre_code = pre_code ^ gtype[i];
-			if (pre_code) {
-				decoded_value += gap / pow(2, position); /* 最上位から順に、最大値と最小値の差の1/2,1/4,1/8,1/16,,,となる */
-			}
-			position++;
-			i++;
-		}
+
+	while (i < g.getGCodeLength()) {
+		if (g.gVector[i])
+			decoded_value += gap / pow(2, i+1);
+		i++;
 	}
-	/* バイナリコードの時 */
-	else {
-		while (i < code_length) {
-			if (gtype[i]) {
-				decoded_value += gap / pow(2, position); /* 最上位から順に、最大値と最小値の差の1/2,1/4,1/8,1/16,,,となる */
-			}
-			position++;
-			i++;
-		}
+
+	p = decoded_value;
+}
+
+void Ptype::setRange(double min, double max) {
+	pMin = min;
+	pMax = max;
+}
+
+/*
+ * individual
+ */
+class GaIndividual {
+public:
+	GaIndividual() {};
+	
+	void setGaIndividual(int gtypeCodeLength, int gtypeCodeMax, double ptyeMin, double ptypeMax);
+	void calcFitness(double p);
+
+	static bool compareGaIndividualPredicate(GaIndividual a, GaIndividual b) { return (a.fitness > b.fitness); }
+
+	double getFitness() { return fitness; }
+	int getRank() { return rank; }
+	int getParent1() { return parent1; }
+	int getParent2() { return parent2; }
+	int getCrossPoint() { return crossPoint; }
+	void setRank(int gRank) { rank = gRank; }
+	void setParent1(int p1) { parent1 = p1; }
+	void setParent2(int p2) { parent2 = p2; }
+	void setCrossPoint(int cp) { crossPoint = cp; }
+
+	Gtype gtype; // Geno Type
+	Ptype ptype; // Pheno Type
+
+private:
+	double fitness;
+	int rank;  // rank after sorting
+	int parent1; // index of parent 1
+	int parent2; // index of parent 2
+	int crossPoint; // crossover point
+};
+
+void GaIndividual::setGaIndividual(int gtypeCodeLength, int gtypeCodeMax, double ptyeMin, double ptypeMax){
+	rank = 0;
+	parent1 = 0;
+	parent2 = 0;
+	crossPoint = 0;
+
+	gtype.setGtypeRamdom(gtypeCodeLength,gtypeCodeMax);
+	ptype.setRange(ptyeMin, ptypeMax);
+}
+
+void GaIndividual::calcFitness(double p) {
+	fitness = gy(fx(p));
+}
+
+/*
+ * a generation
+ */
+class GaGenaration {
+public:
+	GaGenaration() {};
+
+	void initGeneration(int gaPopulationSize, int gtypeCodeLength, int gtypeCodeMax, double ptyeMin, double ptypeMax);
+	void coutResult(int num);
+	int getPopulation() { return static_cast<int>(genes.size()); }
+
+	void evaluation();
+	void selection();
+	void crossover();
+	void mutation();
+
+private:
+
+	GaIndividual decideParentRoulette(std::vector<GaIndividual>& candidate);
+	GaIndividual getChildrenCrossOver(GaIndividual& parent1, GaIndividual& parent2);
+
+	std::vector<GaIndividual> genes;
+	int mutateCount;  // total number of mutation
+	double maxFitness;
+	double minFitness;
+	double avgFitness;
+};
+
+void GaGenaration::initGeneration(int gaPopulationSize, int gtypeCodeLength, int gtypeCodeMax, double ptyeMin, double ptypeMax){
+	mutateCount = 0;
+	maxFitness = 0.0;
+	minFitness = 0.0;
+	avgFitness = 0.0;
+	genes.resize(gaPopulationSize);
+
+	for (int i = 0; i < getPopulation(); i++)
+		genes[i].setGaIndividual(gtypeCodeLength, gtypeCodeMax, ptyeMin, ptypeMax);
+}
+
+void GaGenaration::evaluation() {
+	// calc ptype from gtype
+	for (int i = 0; i < getPopulation(); i++)
+		genes[i].ptype.decodeGtype(genes[i].gtype);
+}
+
+void GaGenaration::selection(){
+	// calc fitness of each genes
+	avgFitness = 0.0;
+	for (int i = 0; i < getPopulation(); i++) {
+		genes[i].calcFitness(genes[i].ptype.getPtype());
+		avgFitness += genes[i].getFitness();
 	}
-	return decoded_value;
+	avgFitness /= genes.size();
+
+	// sort genes by fitness
+	std::sort(genes.begin(), genes.end(), GaIndividual::compareGaIndividualPredicate);
+
+	// identify genes
+	for (int i = 0; i < getPopulation(); i++)
+		genes[i].setRank(i);
+
+	// calc min and max of fitness
+	maxFitness = genes[0].getFitness();
+	minFitness = genes[getPopulation() - 1].getFitness();
+}
+
+void GaGenaration::coutResult(int num) {
+
+	std::cout << "----------------------------------------------------" << std::endl;
+	std::cout << "###  parent   xsite  gtype                    ptype   fitness" << std::endl;
+	int count = 0;
+	for (auto gene : genes) {
+		std::cout << std::setw(3) << count << " (" << std::setw(3) << gene.getParent1() << "," << std::setw(3) << gene.getParent2() << ")  " << std::setw(3) << gene.getCrossPoint() << "   [";
+		for (int i = 0; i < gene.gtype.getGCodeLength(); i++)
+			std::cout << gene.gtype.gVector[i];
+		std::cout << "] " << std::setw(6) << gene.ptype.getPtype() << "    " << std::setw(6) << gene.getFitness() << std::endl;
+		count++;
+	}
+	std::cout << std::endl << "Mutation: " << mutateCount << std::endl
+		<< num << ", " << maxFitness << ", " << avgFitness << ", " << minFitness
+		<< ", " << genes[0].ptype.getPtype() << ", [";
+	for (int i = 0; i < genes[0].gtype.getGCodeLength(); i++)
+		std::cout << genes[0].gtype.gVector[i];
+	std::cout << "]" << std::endl;
+}
+
+void GaGenaration::crossover(){
+	// Reincarnation of Elite
+	for (int i = 0; i < getPopulation()*ELITE_RATE; i++) {
+		genes[i].setParent1(0);
+		genes[i].setParent2(0);
+		genes[i].setCrossPoint(0);
+	}
+
+	// decide parent
+	std::vector<GaIndividual> parent1;
+	std::vector<GaIndividual> parent2;
+	GaIndividual p2Candidate;
+	for (int i = 0; i < getPopulation()*(1.0 - ELITE_RATE); i++) {
+		parent1.push_back(decideParentRoulette(genes));
+		p2Candidate = decideParentRoulette(genes);
+		while (p2Candidate.getRank() == parent1[i].getRank()) {
+			p2Candidate = decideParentRoulette(genes);
+		}
+		parent2.push_back(p2Candidate);
+	}
+
+	//std::cout << std::endl << parent1.size() << ", " << parent1.size() << std::endl;
+
+	// cross over parent
+	std::vector<GaIndividual> children;
+	for (int i = 0; i < getPopulation()*(1.0 - ELITE_RATE); i++)
+		children.push_back(getChildrenCrossOver(parent1[i], parent2[i]));
+
+	// modify generation
+	int numOfChildren = static_cast<int>(children.size());
+	for (int i = 0; i < numOfChildren; i++)
+		genes[getPopulation() - children.size() + i] = children[i];
+}
+
+void GaGenaration::mutation(){
+	mutateCount = 0;
+	for (int i = 0; i < getPopulation()*(1.0 - ELITE_RATE); i++)
+		mutateCount += genes[getPopulation()*ELITE_RATE +i].gtype.mutateGtype();
+}
+
+GaIndividual GaGenaration::decideParentRoulette(std::vector<GaIndividual>& candidate){
+	double r = (double)rand() / RAND_MAX; // 0~1
+	std::vector<double> fitnessCategory;
+	double totalFitness;
+	int numOfCandidate = static_cast<int>(candidate.size());
+
+	// calc fitness category and total fitness
+	fitnessCategory.push_back(0.0);
+	for (int i = 0; i < numOfCandidate; i++)
+		fitnessCategory.push_back(fitnessCategory[i] + genes[i].getFitness());
+	totalFitness = fitnessCategory[numOfCandidate];
+
+	// decide parent
+	for (int i = 0; i < numOfCandidate; i++)
+		if ((r >= fitnessCategory[i] / totalFitness) && (r < fitnessCategory[i + 1] / totalFitness))
+			return candidate[i];
+
+	return GaIndividual();
+}
+
+GaIndividual GaGenaration::getChildrenCrossOver(GaIndividual& parent1, GaIndividual& parent2){
+	GaIndividual child;
+	child = parent1;
+
+	//set parent data
+	child.setParent1(parent1.getRank());
+	child.setParent2(parent2.getRank());
+
+	//decide intersection ramdomely
+	int intersection;
+	intersection = (int)(parent1.gtype.getGCodeLength() * rand() / RAND_MAX);
+	child.setCrossPoint(intersection);
+
+	//calc new gtype
+	for (int i = 0; i < child.gtype.getGCodeLength(); i++)
+		if (i < intersection)
+			child.gtype.gVector[i] = parent2.gtype.gVector[i];
+
+	return child;
 }
 
 #endif // GA_UTIL_H
